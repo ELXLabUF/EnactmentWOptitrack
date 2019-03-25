4e7fa78043404d3fb05a724f1ef7e6f0e734611b
Zarei <n.zarei.3001@tamu.edu>
2019-03-24 23:35:45 -0500

    Finally fixed the unnatural head and torso bend

--- EnactmentInterface_Final/Assets/Scripts/Character.cs
+++ EnactmentInterface_Final/Assets/Scripts/Character.cs
@@ -13,23 +13,24 @@ public class Character : MonoBehaviour {
     // Use this for initialization
     void Start () {
         ik = GetComponent<FullBodyBipedIK>();
-        ik.solver.bodyEffector.target = GameObject.Find("Torso").transform;
+        ik.solver.bodyEffector.target = GameObject.Find("Torso_RB").transform;
         ik.solver.bodyEffector.positionWeight = 1;
-        ik.solver.rightHandEffector.target = GameObject.Find("RightHand").transform;
+        ik.solver.rightHandEffector.target = GameObject.Find("RightHand_RB").transform;
         ik.solver.rightHandEffector.positionWeight = 1;
-        ik.solver.leftHandEffector.target = GameObject.Find("LeftHand").transform;
+        ik.solver.leftHandEffector.target = GameObject.Find("LeftHand_RB").transform;
         ik.solver.leftHandEffector.positionWeight = 1;
-        ik.solver.rightFootEffector.target = GameObject.Find("RightFoot").transform;
+        ik.solver.rightFootEffector.target = GameObject.Find("RightFoot_RB").transform;
         ik.solver.rightFootEffector.positionWeight = 1;
-        ik.solver.leftFootEffector.target = GameObject.Find("LeftFoot").transform;
+        ik.solver.leftFootEffector.target = GameObject.Find("LeftFoot_RB").transform;
         ik.solver.leftFootEffector.positionWeight = 1;
     }
 	
 	// Update is called once per frame
 	void Update () {
         var rotationVector = this.gameObject.GetComponent<Transform>().rotation.eulerAngles;
-        rotationVector.y = GameObject.Find("Torso").transform.rotation.eulerAngles.y;
+        rotationVector.y = GameObject.Find("Torso_RB").transform.rotation.eulerAngles.y;
         this.gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(rotationVector);
+        this.gameObject.GetComponent<Transform>().position = GameObject.Find("Torso_RB").transform.position;
 
     }
 }
